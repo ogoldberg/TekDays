04493587b0e229dec10183b46f00cead67522d0c
oren <ogoldberg@ogoldberg-laptop.(none)>
2010-01-09 21:26:23 -0600

    Fixed test database issues by switching back to Hsql changed EhCacheProvider

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,30 +1,35 @@
 dataSource {
 	pooled = true
-	driverClassName = "com.mysql.jdbc.Driver"
-	username = "root"
+    driverClassName = "com.mysql.jdbc.Driver"
+    username = "root"
 	password = "foo"
 }
 hibernate {
     cache.use_second_level_cache=true
     cache.use_query_cache=true
-    cache.provider_class='com.opensymphony.oscache.hibernate.OSCacheProvider'
+    cache.provider_class='net.sf.ehcache.hibernate.EhCacheProvider'
 }
 // environment specific settings
 environments {
 	development {
 		dataSource {
+            driverClassName = "com.mysql.jdbc.Driver"
 			dbCreate = "update" // one of 'create', 'create-drop','update'
-                        url = "jdbc:mysql://localhost:3306/tekDaysDev"
+            url = "jdbc:mysql://localhost:3306/tekDaysDev"
                 }
 	}
 	test {
 		dataSource {
+            driverClassName = "org.hsqldb.jdbcDriver"
+            username = "sa"
+            password = ""
 			dbCreate = "update"
-			uurl = "jdbc:mysql://localhost:3306/tekDaysTest"
+			url = "jdbc:hsqldb:mem:testDb"
 		}
 	}
         production {
 		dataSource {
+            driverClassName = "com.mysql.jdbc.Driver"
 			dbCreate = "update"
 			url = "jdbc:mysql://localhost:3306/tekdays"
 		}
