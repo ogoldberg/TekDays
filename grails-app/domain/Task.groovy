9dbebdcfbf4ff64c56647ec48019e6b26842a8e3
oren <ogoldberg@ogoldberg-laptop.(none)>
2010-01-11 19:10:29 -0600

    Made it so the title of a task is what appears, instead of task id.

--- grails-app/domain/Task.groovy
+++ grails-app/domain/Task.groovy
@@ -5,12 +5,19 @@ class Task {
     Date dueDate
     TekEvent event
 
+    String toString(){
+        title
+    }
+    
+
     static constraints = {
         title(blank:false)
         notes(blank:true, nullable:true, maxSize:5000)
         assignedTo(nullable:true)
         dueDate(nullable:true)
     }
+
+    
 
     static belongsTo = TekEvent
 }
